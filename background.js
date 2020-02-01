1cb4fea9568cce852649511ef73a649e2d09df4a
Andrew Clark <ac8774@gmail.com>
2020-02-01 14:14:24 -0600

    resizeable canvas

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 function clearScreen(){
     ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.scale(ctx.canvas.width/640, ctx.canvas.height/480)
+    ctx.clearRect(0, 0, 640, 480);
 }
 
 function renderBackground(){
     ctx.save();
     ctx.translate(-camx, 0);
-    ctx.drawImage(sprites["factory_background"],0,0,640*5,360);
+    ctx.drawImage(sprites["factory_background"],0,0,640*5,480);
     ctx.restore();
 }
