58e0afea4ba1c6a223e9d4d6391804549ae4a28b
dandarnell <dandarnell@users.noreply.github.com>
2020-02-01 17:39:53 -0600

    Updated onClick button handlers

--- game_objects/icon_button.js
+++ game_objects/icon_button.js
@@ -1,21 +1,34 @@
 class IconButton extends Button{
-    constructor(icon,x,y, callback){
+    constructor(icon,x,y,callback){
         super(callback);
-        //Dialog title
+        
+        //Button icon
         this.icon=icon;
 
         //Coordinates of top-left corner
-        this.x=x;
-        this.y=y;
-        this.w = sprites["ic/generic_garbage"].width;
-        this.h = sprites["ic/generic_garbage"].height;
+        this.x = x;
+        this.y = y;
+        //this.w = sprites["ic/generic_garbage"].width;
+        //this.h = sprites["ic/generic_garbage"].height;
+        
+        //Button image
+        this.img="icon_button";
     }
 
     render(){
-        drawImage("icon_button",this.x,this.y,1);
+        drawImage(this.img,this.x,this.y,1);
         drawImage("ic/generic_garbage",this.x+8,this.y+8,1);
-        //this.shadow=this.shadow*0.9+10*0.1;
-        //this.x=this.x*0.9-10*0.1;
-        //this.y=this.y*0.9-10*0.1;
+    }
+    
+    onClickDown(){
+        if(this.mouseover)
+            this.img="icon_button_pressed";
+    }
+
+    onClickUp(){
+        if(!this.mouseover){
+            this.img="icon_button";
+            this.callback();
+        }
     }
 }
