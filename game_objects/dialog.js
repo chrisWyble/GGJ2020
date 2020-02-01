1acdb35a16894ddaca8279bb1330b24a58d7cb78
Andrew Clark <ac8774@gmail.com>
2020-02-01 11:18:03 -0600

    preload all images

--- game_objects/dialog.js
+++ game_objects/dialog.js
@@ -2,19 +2,15 @@ class Dialog{
     constructor(title,x,y,w,h){
         //Dialog title
         this.title=title;
-        
+
         //Coordinates of top-left corner
         this.x=x;
         this.y=y;
-        
+
         //Width and height
         this.w=w;
         this.h=h;
-        
-        var img = new Image();
-        img.onload = function(){
-            ctx.drawImage(img,x,y,w,h);
-        };
-        img.src = "logo.png";
+
+        ctx.drawImage(sprites["dialog_background"],x,y,w,h);
     }
 }
--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 var ctx;
-start();
+var sprites;
+init();
 
 function gameLoop() {
     // do stuff
     window.requestAnimationFrame(gameLoop);
 }
 
-function start(){
+function init(){
     cvs = document.getElementById("gameCanvas");
     ctx = cvs.getContext('2d');
+    loadImages()
+}
+
+function start(){
     window.requestAnimationFrame(gameLoop);
+    d = new Dialog("",0,0,100,100)
+}
+
+function loadImages() {
+
+    var names = ['dialog_background'];
+
+    var count  = names.length;
+    var loaded = function() { if (--count == 0) start(); };
+
+    sprites = {}
+    for(var n = 0 ; n < names.length ; n++) {
+        var name = names[n];
+        sprites[name] = new Image()
+        sprites[name].onload = loaded;
+        sprites[name].src = "img/" + name + ".svg";
+    }
 }
