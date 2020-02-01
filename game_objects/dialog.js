5e50bdc5dcc3e0d9c869d5c47920d7a9ec4019b6
dandarnell <dandarnell@users.noreply.github.com>
2020-02-01 11:07:24 -0600

    Changed main

--- game_objects/dialog.js
+++ game_objects/dialog.js
@@ -1,20 +1,20 @@
 class Dialog{
     constructor(title,x,y,w,h){
         //Dialog title
-        this.title = title;
+        this.title=title;
         
         //Coordinates of top-left corner
-        this.x = x;
-        this.y = y;
+        this.x=x;
+        this.y=y;
         
         //Width and height
-        this.w = w;
-        this.h = h;
+        this.w=w;
+        this.h=h;
         
         var img = new Image();
-        img.src = "../img/dialog_background.svg";
-        ctx.drawImage(img,x,y,w,h);
+        img.onload = function(){
+            ctx.drawImage(img,x,y,w,h);
+        };
+        img.src = "logo.png";
     }
 }
-
-var dlg = new Dialog("Hello, world!",50,50,590,310);
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var ctx;
 start();
 
-
 function gameLoop() {
     // do stuff
     window.requestAnimationFrame(gameLoop);
 }
 
 function start(){
-    ctx = gameCanvas.getContext('2d');
+    cvs = document.getElementById("gameCanvas");
+    ctx = cvs.getContext('2d');
     window.requestAnimationFrame(gameLoop);
 }
