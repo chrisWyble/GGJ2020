d40ea1d22b733452adcbc24927e62eb6cfc5fc22
dandarnell <dandarnell@users.noreply.github.com>
2020-02-01 10:46:52 -0600

    .

--- game_objects/dialog.js
+++ game_objects/dialog.js
@@ -1,14 +1,20 @@
-class Dialog {
-    var title; //Dialog title
-    var x,y;   //Coordinates of top-left corner
-    var w,h;   //Width and height
-    
+class Dialog{
     constructor(title,x,y,w,h){
+        //Dialog title
+        this.title = title;
+        
+        //Coordinates of top-left corner
         this.x = x;
         this.y = y;
+        
+        //Width and height
         this.w = w;
         this.h = h;
+        
+        var img = new Image();
+        img.src = "../img/dialog_background.svg";
+        ctx.drawImage(img,x,y,w,h);
     }
-    
-    
 }
+
+var dlg = new Dialog("Hello, world!",50,50,590,310);
