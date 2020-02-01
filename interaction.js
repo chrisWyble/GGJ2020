8cc9474b8b11da726135debd1a6a95e0ba7be315
Andrew Clark <ac8774@gmail.com>
2020-02-01 10:31:27 -0600

    Canvas ctx test

--- interaction.js
+++ interaction.js
@@ -1,10 +1,16 @@
-gameCanvas.addEventListener("click", handleClick);
+gameCanvas.addEventListener("click", handleClickEvent);
 
 function handleClickEvent(e){
     e.preventDefault();
-    handleClick(e.offsetx, e.offsety);
+    handleClick(e.offsetX, e.offsetY);
 }
 
 function handleClick(x, y){
-
+    ctx.strokeStyle = "#F00";
+    ctx.lineWidth = 3.0;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, 2 * Math.PI);
+    ctx.stroke();
 }
--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-
+start();
 
 
 function gameLoop() {
     // do stuff
     window.requestAnimationFrame(gameLoop);
 }
-window.requestAnimationFrame(gameLoop);
+
+function start(){
+    var ctx = gameCanvas.getContext('2d');
+    window.requestAnimationFrame(gameLoop);
+}
