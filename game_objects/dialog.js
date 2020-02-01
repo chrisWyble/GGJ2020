ff02c0304f39df742aa53fa0b5e721a06d88f9e6
dandarnell <dandarnell@users.noreply.github.com>
2020-02-01 10:30:09 -0600

    Added game objects folder and dialog

---
+++ game_objects/dialog.js
@@ -0,0 +1,14 @@
+class Dialog {
+    var title; //Dialog title
+    var x,y;   //Coordinates of top-left corner
+    var w,h;   //Width and height
+    
+    constructor(title,x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    
+    
+}
