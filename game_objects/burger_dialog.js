d334d8e0cffca693d2fee4481d5ef11aa7747971
Andrew Clark <ac8774@gmail.com>
2020-02-01 18:19:52 -0600

    12 buttons

--- game_objects/burger_dialog.js
+++ game_objects/burger_dialog.js
@@ -2,10 +2,17 @@ class BurgerDialog extends Dialog{
     constructor(){
         super("Design your burger!", 30, 30);
 
-        this.icBtn=new IconButton("generic_garbage",50,50, function(){});
-        this.txtBtn=new TextButton("DONE",20,340, function(){dlg = undefined;});
+        this.btnDone=new TextButton("DONE",20,340, function(){dlg = undefined;});
+        this.buttons.push(this.btnDone);
 
-        this.buttons.push(this.icBtn);
-        this.buttons.push(this.txtBtn);
+        var b;
+        var x = 290;
+        var y = 20;
+        ingredientNames.forEach(name => {
+            b = new IconButton(name,x,y, function(){alert(this.icon)});
+            x += 70;
+            if(x>500){x = 290; y += 70;}
+            this.buttons.push(b);
+        });
     }
 }
